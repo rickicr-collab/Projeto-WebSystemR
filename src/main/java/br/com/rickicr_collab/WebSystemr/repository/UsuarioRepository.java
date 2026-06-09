7f0b58216c73210413b7de8d83e07f066cf066fa
Ricardo Rosendo <ricardo_rosendo@ymail.com>
2026-06-09 12:19:39 -0300

    finalized first configuration about bcrypt/passwordEncoder and integration with UsuarioService

--- src/main/java/br/com/rickicr_collab/WebSystemr/repository/UsuarioRepository.java
+++ src/main/java/br/com/rickicr_collab/WebSystemr/repository/UsuarioRepository.java
@@ -3,10 +3,11 @@ package br.com.rickicr_collab.WebSystemR.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.rickicr_collab.WebSystemR.entity.Usuario;
 
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByLogin(String login);
